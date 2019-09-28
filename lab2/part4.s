6a565f7b693e5572b9821680b44b0ebd7698b8ca
Christos-efthymios Kotseldis <mbaxeck4@kilburn.cs.man.ac.uk>
2019-09-28 16:33:32 +0100

    [Lab 2] Added larifications

--- lab2/part4.s
+++ lab2/part4.s
@@ -9,13 +9,13 @@ are	DEFB "you are \0"
 this	DEFB " this year\n\0"
 	ALIGN
 
-	; present = 2005
-	; birth = 1959
-	; year
-	; age = 1
+	; present = 2005 // Change this code
+	; birth = 1959   // Change this code
+	; year = 0       // Change this code
+	; age = 1        // Change this code
 
 main
-	; this code does print "you were born in " + str(birth)
+	; this code does print "you were born in " + str(birth) // DO NOT change the instructions below (except for part 5)
 	ADR R0, born
 	SVC 3
 	LDR R0, birth ; make sure this will work!
@@ -23,11 +23,11 @@ main
 	MOV R0, #10
 	SVC 0
 
-	; year = birth + 1
+	; year = birth + 1 // Change this code
 
-	; while year != present //{
+	; while year != present //{ Change this code
 
-	; this code does print "you were " + str(age) + " in " + str(year)
+	; this code does print "you were " + str(age) + " in " + str(year) // DO NOT change the instructions below (except for part 5)
 	ADR R0, were
 	SVC 3
 	LDR R0, age ; make sure this will work!
@@ -39,11 +39,11 @@ main
 	MOV R0, #10
 	SVC 0
 
-	;   year = year + 1;
-	;   age = age + 1;
-	; //}
+	;   year = year + 1 //Change this code
+	;   age = age + 1   //Change this code
+	; } //              //Change this code
 
-	; this code does print "you are " + str(age) + "this year"
+	; this code does print "you are " + str(age) + "this year" // DO NOT change the instructions below (except for part 5)
 	ADR R0, are
 	SVC 3
 	LDR R0, age ; make sure this will work!
